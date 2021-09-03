ba97c96480c607cb984811bad2ff1bb6cf6f1b21
Scotty Wyatt <crashy@scottys-mbp.attlocal.net>
2021-09-03 09:25:56 -0400

    used js to start setting up the game board

--- main.js
+++ main.js
@@ -1,3 +1,47 @@
 let grid = document.querySelector(".grid");
 let popup = document.querySelector(".popup");
-let playAgain = document.querySelector(".playAgain")
+let playAgain = document.querySelector(".playAgain");
+let scoreDisplay = document.querySelector(".scoreDisplay");
+let left = document.querySelector(".left");
+let right = document.querySelector(".right");
+let up = document.querySelector(".up");
+let down = document.querySelector(".down");
+let width = 10;
+let currentIndex = 0;
+let appleIndex = 0;
+let currentSnake = [2, 1, 0];
+let direction = 1;
+let score = 0;
+let speed = 0.8;
+let intervalTime = 0;
+let interval = 0;
+
+document.addEventListener("DOMContentLoaded", function () {
+  document.addEventListener("keyup", control);
+  createBoard();
+  startGame();
+  playAgain.addEventListener("click", replay);
+});
+
+
+function createBoard() {
+  popup.style.display = 'none';
+  for(let i = 0; i < 100; i++) {
+    let div = document.createElement('div');
+    grid.appendChild(div);
+  }
+}
+
+
+function startGame() {
+  let squares = document.querySelector(".grid div");
+  randomApples(squares);
+  //random apple
+  direction = 1;
+  scoreDisplay.innerHTML = score;
+  intervalTime = 1000;
+  currentSnake = [2, 1, 0];
+  currentIndex = 0;
+  currentSnake.forEach((index) => squares[index].classList.add("snake"));
+  interval = setInterval(moveOutcome, intervalTime);
+}
